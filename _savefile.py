5337cfa0069d4cb7fc2c49cc48a3e250c43400ad
Nityanand Waingankar <52778038+nityanandwaingankar@users.noreply.github.com>
2023-01-24 11:58:34 +0530

    added via upload

---
+++ _savefile.py
@@ -0,0 +1,16 @@
+# # save numpy array as npy file
+# from numpy import asarray
+# from numpy import save, load
+# # define data
+# data = asarray([[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]])
+# # save to npy file
+# save("Inputdata/data.npy", data)
+
+
+# returned = load('Inputdata/data.npy')
+
+# print(returned)
+
+# val=returned==data
+# if val.all():
+#     print("success")
